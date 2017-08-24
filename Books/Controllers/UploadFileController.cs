3f2b112aad739d91215a782c2ad8a85f1246db8a
Anchal Chaudhary <anchal.ldn5@gmail.com>
2017-08-24 15:38:46 +0530

    Excels compared, new excel generated

--- Books/Controllers/UploadFileController.cs
+++ Books/Controllers/UploadFileController.cs
@@ -6,6 +6,9 @@ using System.Web.Mvc;
 using Books.Models;
 using System.Runtime.InteropServices;
 using Excel = Microsoft.Office.Interop.Excel;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Web.UI;
 
 namespace Books.Controllers
 {
@@ -21,9 +24,13 @@ namespace Books.Controllers
         [HttpPost]
         public ActionResult Index(HttpPostedFileBase excelBooks, HttpPostedFileBase excelBarcode)
         {
+            bool isPresent = false;
             if (excelBooks.FileName.EndsWith("xls") || excelBooks.FileName.EndsWith("xlsx") && excelBarcode.FileName.EndsWith("xls") || excelBarcode.FileName.EndsWith("xlsx"))
             {
+                //path for Books excel
                 string pathBooks = Server.MapPath("~/UploadedExcel/" + excelBooks.FileName);
+
+                //path for Barcode excel
                 string pathBarcode = Server.MapPath("~/UploadedExcel/" + excelBarcode.FileName);
 
                 if (System.IO.File.Exists(pathBooks))
@@ -37,18 +44,26 @@ namespace Books.Controllers
                     System.IO.File.Delete(pathBarcode);
                 }
                 excelBarcode.SaveAs(pathBarcode);
+
                 Excel.Application application = new Excel.Application();
+
+                //Books
                 Excel.Workbook workbookBooks = application.Workbooks.Open(pathBooks);
                 Excel.Worksheet worksheetBooks = workbookBooks.ActiveSheet;
                 Excel.Range rangeBooks = worksheetBooks.UsedRange;
 
+                //Barcode
                 Excel.Workbook workbookBarcode = application.Workbooks.Open(pathBarcode);
                 Excel.Worksheet worksheetBooksBarcode = workbookBarcode.ActiveSheet;
                 Excel.Range rangeBarcode = worksheetBooksBarcode.UsedRange;
 
+                //List for books
                 List<tblBook> datatblBooks = new List<tblBook>();
+
+                //List for barcodes
                 List<tblBarcode> datatblBarcode = new List<tblBarcode>();
 
+                //importing books excel data into list for books
                 for (int row = 1; row <= rangeBooks.Rows.Count; row++)
                 {
                     if (true)
@@ -64,6 +79,7 @@ namespace Books.Controllers
                     }
                 }
 
+                //importing barcode excel data into list for barcode
                 for (int row = 1; row <= rangeBarcode.Rows.Count; row++)
                 {
                     if (true)
@@ -74,50 +90,64 @@ namespace Books.Controllers
                         });
                     }
                 }
-                //using (DBBooksEntities db = new DBBooksEntities())
-                //{
-                //    foreach (var item1 in datatblBooks)
-                //    {
-                //        tblBook objtblBook = new tblBook();
-                //        objtblBook.barcode = item1.barcode;
-                //        objtblBook.itemcallnumber = item1.itemcallnumber;
-                //        objtblBook.author = item1.author;
-                //        objtblBook.title = item1.title;
-                //        objtblBook.publishercode = item1.publishercode;
 
-                //        db.tblBooks.Add(objtblBook);
-                //        db.SaveChanges();
-                //    }
-                //    foreach (var item2 in datatblBarcode)
-                //    {
-                //        tblBarcode objtblBarcode = new tblBarcode();
-                //        objtblBarcode.Barcode = item2.Barcode;
+                
 
-                //        db.tblBarcodes.Add(objtblBarcode);
-                //        db.SaveChanges();
-                //    }
-                //}
-
-                foreach (var item2 in datatblBarcode)
+                //list for books with no barcode in datatblBarcode 
+                List<tblBook> tblBooksList = new List<tblBook>();
+                
+                List<tblBook> newList = new List<tblBook>();
+                newList.Add(new tblBook
                 {
-                    foreach (var item1 in datatblBooks)
+                    barcode = "",
+                    itemcallnumber = "",
+                    author = "",
+                    title = "",
+                    publishercode = "",
+                });
+
+                //check if every book has it's barcode in list of barcodes
+                foreach (var itemBook in datatblBooks)
+                {
+                    isPresent = false;
+                    foreach (var itemBarcode in datatblBarcode)
                     {
-                        if (item1.barcode != item2.Barcode)
+                        if (itemBook.barcode == itemBarcode.Barcode)
                         {
-                            tblBook objtblBook = new tblBook();
-                            objtblBook.barcode = item1.barcode;
-                            objtblBook.title = item1.title;
-                            objtblBook.itemcallnumber = item1.itemcallnumber;
-                            objtblBook.publishercode = item1.publishercode;
-
-
-                        }
-                        else
-                        {
+                            isPresent = true;
                             break;
                         }
                     }
+                    if (isPresent == false)
+                    {
+                        if (itemBook.barcode != "barcode")
+                        {
+                            tblBook objtblBook = new tblBook();
+                            objtblBook.barcode = itemBook.barcode;
+                            objtblBook.itemcallnumber = itemBook.itemcallnumber;
+                            objtblBook.author = itemBook.author;
+                            objtblBook.title = itemBook.title;
+                            objtblBook.publishercode = itemBook.publishercode;
+
+                            tblBooksList.Add(objtblBook);
+                        }
+                    }
                 }
+
+                var gridView = new GridView();
+                gridView.DataSource = tblBooksList;
+                gridView.DataBind();
+                Response.ClearContent();
+                Response.Buffer = true;
+                Response.AddHeader("content-disposition", "attachment; filename=ListOfBooks.xls");
+                Response.ContentType = "application/ms-excel";
+                Response.Charset = "";
+                StringWriter objStringWriter = new StringWriter();
+                HtmlTextWriter objHtmlTextWriter = new HtmlTextWriter(objStringWriter);
+                gridView.RenderControl(objHtmlTextWriter);
+                Response.Output.Write(objStringWriter.ToString());
+                Response.Flush();
+                Response.End();
                 return View("Index");
             }
             return View("Index");
